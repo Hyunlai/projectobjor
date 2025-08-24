1a389777012424eb3dfef3f4b36743f5f39a32e1
Nibe <neabella2@student.hau.edu.ph>
2025-08-25 02:06:29 +0800

    Made clickable Profiles through Posts

--- Base/views.py
+++ Base/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from Posts.models import Post
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def home(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'Base/home.html', {'posts': posts})
